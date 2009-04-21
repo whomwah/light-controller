046025e93a6791b681f93c4587d00954ec9e32a8
Duncan Robertson <duncan@whomwah.com>
2009-04-21 10:24:58 +0100

    updates, almost ready

--- app.rb
+++ app.rb
@@ -1,23 +1,25 @@
-require 'rubygems'
 require 'osc'
-
-configure do
-  LIGHTS_PROXY = 'http://10.54.146.25'
-end
+#module OSC
+#  class UDPSocket
+#    def initialize
+#    end
+#    def connect(a,b)
+#    end
+#  end
+#end
 
 helpers do
   class LC 
-    LIGHTS = [1,2]
-
-    def self.set_lights(options=nil)
+    def self.set_lights(opts=nil)
       sock = OSC::UDPSocket.new
       sock.connect('10.54.146.25', 7770)
 
-      colour = options[:colour] if options.is_a?(Hash) && options.has_key?(:colour)
-      lights = options[:lights] if options.is_a?(Hash) && options.has_key?(:lights)
+      halt "Need some lights" unless lights = opts.delete(:lights)
+
       result = []
       lights.each do |light|
-        l = Light.new(sock,colour,light)
+        opts[:light] = light
+        l = Light.new(sock, opts)
         result << l.display
       end 
       return result.join
@@ -25,20 +27,13 @@ helpers do
   end
 
   class Light
-    COLOURS = {
-      :red   => [255,0,0],
-      :green => [0,255,0],
-      :blue  => [0,0,255],
-    }
-    CHANNELS = {
-      1 => [1,2,3],
-      2 => [4,5,6],
-    }
+    attr_writer :rgb
 
-    def initialize(sock,colour,light)
-      @colour = colour
-      @light = light.to_i
-      @sock = sock
+    def initialize(socket, opts=nil)
+      @sock = socket 
+      @colour = opts[:colour] if opts.is_a?(Hash) && opts.has_key?(:colour)
+      @light = opts[:light].to_i if opts.is_a?(Hash) && opts.has_key?(:light)
+      @rgb = opts[:rgb]
     end
     
     def display
@@ -51,16 +46,17 @@ helpers do
     end 
 
     def to_s
-      "<p>Setting the color to <em style='#{self.to_css}font-weight:bold;'>#{@colour}</em> for light <em>#{@light}</em></p>"
+      "Setting the color to <em style='#{self.to_css}font-weight:bold;'>#{@colour}</em> for light <em>#{@light}</em>\n"
     end
 
     def colour_channel_hash
-      if COLOURS.has_key?(@colour.to_sym)
-        c = COLOURS[@colour.to_sym] 
+      return [] unless @rgb
+
+      if @rgb.has_key?(@colour.to_sym)
+        c = @rgb[@colour.to_sym] 
       else
         c = @colour.gsub('rgb','').split('-')
       end
-      puts c.inspect
 
       data = []
       (1..3).each do |i|
@@ -72,8 +68,8 @@ helpers do
 
     def to_css
       # color: rgb(51, 51, 51);
-      if COLOURS.has_key?(@colour.to_sym)
-        "color:rgb(#{COLOURS[@colour.to_sym].join(',')});" 
+      if @rgb.has_key?(@colour.to_sym)
+        "color:rgb(#{@rgb[@colour.to_sym].join(',')});" 
       else
         c = @colour.gsub('rgb','').gsub('-',',')
         "color:rgb(#{c});" 
@@ -82,15 +78,30 @@ helpers do
   end
 end
 
+before do
+  @lights = [1,2]
+  @rgb = {
+    :red   => [255,0,0],
+    :green => [0,255,0],
+    :blue  => [0,0,255],
+  }
+  @channels = {
+    1 => [1,2,3],
+    2 => [4,5,6],
+  }
+end
+
 # /all/red
 get %r{/(1|2|all)+/(rgb\d{0,3}-\d{0,3}-\d{0,3}|red|blue|green|random)+$} do
   lights = [params['captures'].first]
-  lights = LC::LIGHTS if lights.include?('all')
+  lights = @lights if lights.include?('all')
   colour = params['captures'][1]
   halt 404, 'Oh dear! I understand what you\'re saying' unless lights && colour 
   LC.set_lights(
     :lights => lights,
-    :colour => colour
+    :colour => colour,
+    :rgb => @rgb,
+    :channels => @channels
   )
 end
 
